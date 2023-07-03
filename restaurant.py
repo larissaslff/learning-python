f8aea64d3e8a904018835f790038da86298097b6
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-07-03 19:48:51 -0300

    add number_served

--- restaurant.py
+++ restaurant.py
@@ -1,12 +1,18 @@
 class Restaurant:
-    def __init__(self, restaurant_name,  cuisine_type):
+    def __init__(self, restaurant_name,  cuisine_type, number_served=0):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = number_served
     
     
     def describe_restaurant(self):
         print(f'Nome do restaurante {self.restaurant_name}')
         print(f'Tipo de cozinha: {self.cuisine_type}')
+        print(f'Numero servido: {self.number_served}')
+    
+    
+    def set_number_served(self, number_served):
+        self.number_served = number_served
     
     
     def open_restaurant(self):
@@ -16,3 +22,5 @@ class Restaurant:
 restaurante = Restaurant('Cozinha lá de casa', 'Caseira')
 restaurante.describe_restaurant()
 restaurante.open_restaurant()
+restaurante.set_number_served(3)
+restaurante.describe_restaurant()
